f23fb3311313961f417846f7ce8c05cbffe081a4
mnAnnaSan <mnannasan@gmail.com>
2021-01-15 18:39:54 +0000

    Sample code

---
+++ js/index.js
@@ -0,0 +1,20 @@
+alert('Hello');
+
+
+let message = 'Hello JavaScript!';
+alert(message);
+
+let name = prompt('Enter you name');
+alert(name);
+
+let age = Number(prompt('Enter you age'));
+alert('You age is ' + age);
+
+age = age + 1; // - /* %
+alert('Next year you will be ' + age);
+
+if (age >= 18) {
+alert('You are adult!');
+}else{
+alert('You are underaged!');
+}
